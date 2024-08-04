45327a54fb934fdd1d9759ddba4b718d8b6fb45c
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-08-05 00:08:34 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the order_details table
