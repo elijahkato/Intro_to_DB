6946cc0c0afbb57fd6b64037c7c3f77ae3d7e037
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-08-05 00:25:15 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -4,4 +4,4 @@ USE alx_book_store;
 -- Print the full description of the table books
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
